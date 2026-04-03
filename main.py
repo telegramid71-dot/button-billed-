c97b37ebe2984e8225d290e9d9ad5ea0e5bd3da6
telegramid71-dot <telegramid71@gmail.com>
2026-04-03 03:51:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import asyncio
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+from pyrogram import Client, filters
 import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
